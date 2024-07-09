04c0df8a0d542b6899ed0c8a748b22f9e2a71847
jungkeunji <gjk0508@gmail.com>
2024-07-09 23:56:11 +0900

    first commit

---
+++ src/main/kotlin/com/mss/brandcategoryproduct/BrandCategoryProductApplication.kt
@@ -0,0 +1,11 @@
+package com.mss.brandcategoryproduct
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrandCategoryProductApplication
+
+fun main(args: Array<String>) {
+    runApplication<BrandCategoryProductApplication>(*args)
+}
---
+++ src/test/kotlin/com/mss/brandcategoryproduct/BrandCategoryProductApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mss.brandcategoryproduct
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BrandCategoryProductApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
